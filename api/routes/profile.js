451086789f4116176568667217e45f1fb1442fe2
Cassy343 <isilversmith810@gmail.com>
2022-05-31 16:48:16 -0400

    Add GET /profile

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -1,9 +1,32 @@
+const { default: axios } = require('axios');
 var express = require('express');
 var router = express.Router();
+const { db } = require('../db');
 require('dotenv').config();
+const { getDocs, doc, query, collection, where } = require('firebase/firestore');
 
-router.get('/', (req, res, next) => {
-    
+const BASE_URL = 'https://api.spotify.com/v1';
+
+router.get('/', (req, res) => {
+    axios.get(`${BASE_URL}/me`, {
+        headers: {
+            'Authorization': `Bearer ${req.query.spotifyToken}`
+        }
+    })
+    .then(res2 => {
+        const spotifyId = res2.data.id;
+        const profileQuery = query(
+            collection(db, 'users'),
+            where('spotifyId', '==', spotifyId)
+        );
+        getDocs(profileQuery).then(docs => {
+            const profileDoc = docs.docs[0];
+            res.send({
+                ...profileDoc.data(),
+                id: profileDoc.id
+            });
+        });
+    });
 });
 
 module.exports = router;
