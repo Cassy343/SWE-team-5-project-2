021e85b58576682b7bfa55b3eec5066d774b27d2
Ashwin <as7nj@virginia.edu>
2022-05-31 16:45:58 -0400

    Initial-Discover-Page

--- frontend/src/discover/Discover.js
+++ frontend/src/discover/Discover.js
@@ -1,6 +1,57 @@
+import React from "react";
+import {useState} from "react";
+import {Typography, Button, Card, CardActionArea, CardActions, CardContent} from "@mui/material";
+import '../discover/Discover.css'
+import Profile from '../profile/Profile'
+
 function Discover(props) {
+    const [IdProfile, setIdProfile] = useState();
+    const [users, setUsers] = useState([{
+        name: 'Ashwin Shankar',
+        song: "It's a killa",
+        artist: 'Glass Animals',
+        userId: '12345'
+    }, 
+    {
+        name:'Spongebob Squarepants',
+        song: 'Krusty Krab Pizza',
+        artist: 'Squidward',
+        userId: '12345'
+    }]) // each user will contain name, top artist, top song, 
+
+
     return (<>
-        <p>Test</p>
+        <div className="content-container">
+            <div className="row">
+                <div className="left-panel box">
+                    {users.map((user) => (
+                        <Card sx={{m: 2}} style={{backgroundColor: "rgb(30,215,96)", opacity: 0.9}}>
+                            <CardActionArea >
+                                <Typography variant="h3" color='black' textAlign='center' >{user.name}</Typography>
+                            </CardActionArea>
+                            <CardContent>
+                                <Typography color='grey' variant='h6'> Favorite Song: {user.song}</Typography>
+                                <Typography color='grey' variant='h6'> Favorite Artist: {user.artist} </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button  sx = {{border: 'black', "&:hover": {border: '1px solid black'}}}fullWidth ='true' variant='outlined' size='small' onClick = { () => {
+                                    setIdProfile(user.userId)
+                                    }}>
+                                        <Typography color ='black' variant='h7'>
+                                            View Profile
+                                        </Typography>
+                                </Button>
+                            </CardActions>
+                                
+                        </Card>
+                    )
+                    )}
+                </div>
+                <div className="right-panel box">
+                    <Profile userId = {IdProfile}></Profile>
+                </div>
+            </div>
+        </div>
     </>);
 }
 
